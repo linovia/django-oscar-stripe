18a111c3d69ab59f59b0a4147000245550ec9b01
Xavier Ordoquy <xordoquy@linovia.com>
2014-02-11 18:44:43 +0100

    Save the stripe token for deferred use.

---
+++ oscar_stripe/models.py
@@ -0,0 +1,23 @@
+
+from django.db import models
+from django.utils.translation import ugettext as _
+
+from oscar.core.compat import AUTH_USER_MODEL
+
+
+class StripeToken(models.Model):
+    """
+    Model representing a Token for strip API
+    """
+    user = models.ForeignKey(AUTH_USER_MODEL,
+        related_name='stripe_token',
+        verbose_name=_("User"))
+    token = models.CharField(max_length=128)
+
+    def __unicode__(self):
+        return u'Token %s' % self.token
+
+    class Meta:
+        abstract = True
+        verbose_name = _("Token")
+        verbose_name_plural = _("Tokens")
